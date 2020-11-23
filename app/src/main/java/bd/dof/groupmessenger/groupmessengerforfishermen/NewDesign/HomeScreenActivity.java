34c9bfc0ac4a65770ca91a30cbbf0c00a00930bd
Tasnuva <tasnuva.oshin12@gmail.com>
2020-11-23 11:49:58 +0600

    header added

---
+++ app/src/main/java/bd/dof/groupmessenger/groupmessengerforfishermen/NewDesign/HomeScreenActivity.java
@@ -0,0 +1,15 @@
+package bd.dof.groupmessenger.groupmessengerforfishermen.NewDesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import bd.dof.groupmessenger.groupmessengerforfishermen.R;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
