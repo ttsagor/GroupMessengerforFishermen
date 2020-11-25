73707ef24791ff3692f9a63d6a2aafaca84e8826
Tasnuva <tasnuva.oshin12@gmail.com>
2020-11-25 12:30:55 +0600

    Login System Done

--- app/src/main/java/bd/dof/groupmessenger/groupmessengerforfishermen/NewDesign/LoginActivity.java
+++ app/src/main/java/bd/dof/groupmessenger/groupmessengerforfishermen/NewDesign/LoginActivity.java
@@ -2,15 +2,35 @@ package bd.dof.groupmessenger.groupmessengerforfishermen.NewDesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import bd.dof.groupmessenger.groupmessengerforfishermen.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText mobileNumber, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mobileNumber = findViewById(R.id.mobile_number);
+        password = findViewById(R.id.password);
+    }
+
+    public void submitNow(View view) {
+        if (!TextUtils.isEmpty(mobileNumber.getText().toString()) && !TextUtils.isEmpty(password.getText().toString())) {
+
+            startActivity(new Intent(LoginActivity.this, HomeScreenActivity.class));
+            LoginActivity.this.overridePendingTransition(0, 0);
+            LoginActivity.this.finish();
+        } else {
+            Toast.makeText(this, "Please Enter Mobile & Password Correctly", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
