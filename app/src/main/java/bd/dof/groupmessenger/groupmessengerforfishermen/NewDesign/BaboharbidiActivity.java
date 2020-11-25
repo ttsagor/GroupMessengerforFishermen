b146c3e33e6e4dc4f9cc7646b3d5d549846360e5
Tasnuva <tasnuva.oshin12@gmail.com>
2020-11-26 00:21:08 +0600

    আপডেট পেইজ এদ্দেদ

---
+++ app/src/main/java/bd/dof/groupmessenger/groupmessengerforfishermen/NewDesign/BaboharbidiActivity.java
@@ -0,0 +1,16 @@
+package bd.dof.groupmessenger.groupmessengerforfishermen.NewDesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import bd.dof.groupmessenger.groupmessengerforfishermen.R;
+
+public class BaboharbidiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baboharbidi);
+    }
+}
---
+++ app/src/main/java/bd/dof/groupmessenger/groupmessengerforfishermen/NewDesign/ComingSoonActivity.java
@@ -0,0 +1,16 @@
+package bd.dof.groupmessenger.groupmessengerforfishermen.NewDesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import bd.dof.groupmessenger.groupmessengerforfishermen.R;
+
+public class ComingSoonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coming_soon);
+    }
+}
