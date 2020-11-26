789f7d25c56c7bb31a82a8f1bc3e9b06e5291232
Tasnuva <tasnuva.oshin12@gmail.com>
2020-11-26 23:15:08 +0600

    login logout system added

--- app/src/main/java/bd/dof/groupmessenger/groupmessengerforfishermen/NewDesign/HomeScreenActivity.java
+++ app/src/main/java/bd/dof/groupmessenger/groupmessengerforfishermen/NewDesign/HomeScreenActivity.java
@@ -4,17 +4,24 @@ package bd.dof.groupmessenger.groupmessengerforfishermen.NewDesign;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import java.util.ArrayList;
+import java.util.Objects;
 
 import bd.dof.groupmessenger.groupmessengerforfishermen.DbHandler;
 import bd.dof.groupmessenger.groupmessengerforfishermen.FarmerInfoModel;
@@ -32,49 +39,103 @@ public class HomeScreenActivity extends AppCompatActivity implements View.OnClic
     public static String upazillaID = "66";
     public static ArrayList<FarmerInfoModel> allFarmerInfo = new ArrayList<FarmerInfoModel>();
     public static ArrayList<UnionModel> allUnion = new ArrayList<UnionModel>();
+    public static String log;
+    private LinearLayout logged_id, loggedout_id;
+    private SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        SharedPreferences pref = HomeScreenActivity.this.getApplicationContext().getSharedPreferences("MyPref", 0);
+        log = pref.getString("log", "");
+        editor = pref.edit();
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 1);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+        logged_id = findViewById(R.id.logged_id);
+        loggedout_id = findViewById(R.id.logged_out);
         findViewById(R.id.ll_mosso_sutro).setOnClickListener(this);
         findViewById(R.id.ll_invention).setOnClickListener(this);
         findViewById(R.id.ll_desease).setOnClickListener(this);
         findViewById(R.id.ll_khude_barta_peron).setOnClickListener(this);
         findViewById(R.id.ll_chasi_onusandhan).setOnClickListener(this);
-        DbHandler db = new DbHandler(this,null,null,1);
+        DbHandler db = new DbHandler(this, null, null, 1);
         SystemInformationModel cSystemInfo = db.getSystemInfo();
         divisionID = cSystemInfo.getUserDivisionID();
         districtID = cSystemInfo.getUserDistrictID();
         upazillaID = cSystemInfo.getUserUpazillaID();
-
         allUnion = new ArrayList<UnionModel>();
         allFarmerInfo = new ArrayList<FarmerInfoModel>();
-        allUnion = db.getAllUnion(HomeScreenActivity.divisionID,HomeScreenActivity.districtID,HomeScreenActivity.upazillaID);
+        allUnion = db.getAllUnion(HomeScreenActivity.divisionID, HomeScreenActivity.districtID, HomeScreenActivity.upazillaID);
         allFarmerInfo = db.getAllFarmerInfoBy();
+        bottomNavigationHandler();
 
+        if (log.equals("true")) {
+            logged_id.setVisibility(View.VISIBLE);
+            loggedout_id.setVisibility(View.GONE);
+        } else {
+            logged_id.setVisibility(View.GONE);
+            loggedout_id.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    private void bottomNavigationHandler() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.getMenu().clear();
+        bottomNavigationView.inflateMenu(R.menu.new_bottom_menu_home);
+        if (log.equals("true")) {
+
+            bottomNavigationView.getMenu().removeItem(R.id.menu_sotorkota);
+
+            bottomNavigationView.getMenu().getItem(3).setTitle("লগআউট");
+        } else {
+            bottomNavigationView.getMenu().removeItem(R.id.menu_profile);
+
+
+
+        }
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+
+                    case R.id.menu_login:
+                        if (log.equals("true")) {
+                            editor.putString("log", "false");
+                            editor.apply();
+                            startActivity(new Intent(HomeScreenActivity.this, SplashScreenActivity.class));
+                            HomeScreenActivity.this.finish();
+                        } else {
+                            startActivity(new Intent(HomeScreenActivity.this, LoginActivity.class));
+                        }
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
     @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.ll_mosso_sutro:
-                startActivity(new Intent(HomeScreenActivity.this,fishFarming.class));
+                startActivity(new Intent(HomeScreenActivity.this, fishFarming.class));
 
                 break;
             case R.id.ll_invention:
-                startActivity(new Intent(HomeScreenActivity.this,InventorInformation.class));
+                startActivity(new Intent(HomeScreenActivity.this, InventorInformation.class));
                 break;
             case R.id.ll_desease:
-                startActivity(new Intent( HomeScreenActivity.this , disease.class));
+                startActivity(new Intent(HomeScreenActivity.this, disease.class));
                 break;
             case R.id.ll_khude_barta_peron:
-                startActivity(new Intent( HomeScreenActivity.this , KhudebartaActivity.class));
+                startActivity(new Intent(HomeScreenActivity.this, KhudebartaActivity.class));
                 break;
             case R.id.ll_chasi_onusandhan:
-                startActivity(new Intent( HomeScreenActivity.this , ChasiOnusandhanActivity.class));
+                startActivity(new Intent(HomeScreenActivity.this, ChasiOnusandhanActivity.class));
                 break;
         }
     }
@@ -83,7 +144,7 @@ public class HomeScreenActivity extends AppCompatActivity implements View.OnClic
     private void requestSmsPermission() {
         String permission = Manifest.permission.RECEIVE_SMS;
         int grant = ContextCompat.checkSelfPermission(this, permission);
-        if ( grant != PackageManager.PERMISSION_GRANTED) {
+        if (grant != PackageManager.PERMISSION_GRANTED) {
             String[] permission_list = new String[1];
             permission_list[0] = permission;
             ActivityCompat.requestPermissions(this, permission_list, 1);
--- app/src/main/java/bd/dof/groupmessenger/groupmessengerforfishermen/NewDesign/LoginActivity.java
+++ app/src/main/java/bd/dof/groupmessenger/groupmessengerforfishermen/NewDesign/LoginActivity.java
@@ -3,28 +3,34 @@ package bd.dof.groupmessenger.groupmessengerforfishermen.NewDesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 import bd.dof.groupmessenger.groupmessengerforfishermen.R;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText mobileNumber, password;
-
+    private SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        SharedPreferences pref = LoginActivity.this.getApplicationContext().getSharedPreferences("MyPref", 0);
+        editor = pref.edit();
         mobileNumber = findViewById(R.id.mobile_number);
         password = findViewById(R.id.password);
     }
 
     public void submitNow(View view) {
         if (!TextUtils.isEmpty(mobileNumber.getText().toString()) && !TextUtils.isEmpty(password.getText().toString())) {
-
+            editor.putString("log", "true");
+            editor.apply();
             startActivity(new Intent(LoginActivity.this, HomeScreenActivity.class));
             LoginActivity.this.overridePendingTransition(0, 0);
             LoginActivity.this.finish();
